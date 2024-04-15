dcd65bac79d7d250ed8fc2f4c00e6ae65944b913
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-04-15 10:14:58 +0530

    Update tt_um_chisel_template.v

--- src/tt_um_chisel_template.v
+++ src/tt_um_chisel_template.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_chisel_template (
+module tt_um_Counter_shivam (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,21 +15,20 @@ module tt_um_chisel_template (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+ 
+    reg[9:0] out;
+ assign rst = rst_n;
+ assign uo_out = out;
 
   // All output pins must be assigned. If not used, assign to 0.
-  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  // assign uio_out = 0;
-  // assign uio_oe  = 0;
-    wire reset = !rst_n;
-    // Just wrap the Chisel generated Verilog
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
-    ChiselTop ChiselTop(.clock(clk),
-      .reset(reset),
-      .io_ui_in(ui_in),
-      .io_uo_out(uo_out),
-      .io_uio_in(uio_in),
-      .io_uio_out(uio_out),
-      .io_uio_oe(uio_oe),
-      .io_ena(ena));
-
+    always@(posedge clk)
+begin 
+    if(!rst)
+out <=0;
+else
+    out<= out +1;
+end
 endmodule
