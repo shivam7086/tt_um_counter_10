5573626047f5cb0ba7cd9e61e5e3ee8a0a28b241
shivam7086 <161806939+shivam7086@users.noreply.github.com>
2024-04-15 18:35:51 +0530

    Update tt_um_Counter.v

--- src/tt_um_Counter.v
+++ src/tt_um_Counter.v
@@ -23,12 +23,45 @@ module tt_um_Counter_shivam (
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
   assign uio_oe  = 0;
+  assign rst_n =rst;
+  assign ui_in[0] =up_dowm;
+   assign ui_in[1]=hold;
+    assign uo_out[0] =out;
+    assign uo_out[1]= out_binary;
+    assign uo_out[2]= out_hexadecimal;
+    assign uo_out[3]= out_decimal;
+    
+// Internal registers
+reg [31:0] count_reg;
+reg [31:0] binary_reg;
+reg [7:0] hexadecimal_reg;
+reg [9:0] decimal_reg;
 
-    always@(posedge clk or posedge rst)
-begin 
-    if( rst)
-out <=0;
-else
-    out<= out +1;
+// Counter logic
+always @(posedge clk or posedge rst)
+begin
+    if (rst)
+        count_reg <= 0;
+    else if (hold)
+        count_reg <= count_reg;
+    else if (up_down)
+        count_reg <= count_reg + 1;
+    else
+        count_reg <= count_reg - 1;
 end
+
+// Convert count to binary, hexadecimal, and decimal
+always @(*)
+begin
+    binary_reg = count_reg;
+    hexadecimal_reg = count_reg;
+    decimal_reg = count_reg;
+end
+
+// Output assignments
+assign out = count_reg;
+assign out_binary = binary_reg;
+assign out_hexadecimal = hexadecimal_reg;
+assign out_decimal = decimal_reg;
+
 endmodule
